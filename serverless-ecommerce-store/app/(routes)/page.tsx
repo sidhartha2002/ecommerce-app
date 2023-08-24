0c38db528fe61cf6180a499522cc5facbe4505f9
sidhartha2002 <mohantysidhartha2002@gmail.com>
2023-08-25 03:45:50 +0530

    fix frontend-store

--- serverless-ecommerce-store/app/(routes)/page.tsx
+++ serverless-ecommerce-store/app/(routes)/page.tsx
@@ -8,20 +8,18 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("a9107d1a-f102-418c-b798-828869ae770e");
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard 
-          data={billboard}
-        />
+        <Billboard data={billboard} />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" items={products} />
         </div>
       </div>
     </Container>
-  )
+  );
 };
 
 export default HomePage;
--- serverless-ecommerce-store/components/footer.tsx
+++ serverless-ecommerce-store/components/footer.tsx
@@ -3,11 +3,11 @@ const Footer = () => {
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
         <p className="text-center text-xs text-black">
-          &copy; 2023 Store, Inc. All rights reserved.
+          &copy; Sdhartha Mohanty eCommerce. All rights reserved.
         </p>
       </div>
     </footer>
-  )
+  );
 };
 
 export default Footer;
